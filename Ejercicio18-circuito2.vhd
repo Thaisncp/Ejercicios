0edad6684d3d2170557f80f7244fff0beaf7590a
Thais Cartuche <93340721+Thaisncp@users.noreply.github.com>
2022-05-18 21:53:38 -0500

    Update Ejercicio18-circuito2.vhd

--- Ejercicio18-circuito2.vhd
+++ Ejercicio18-circuito2.vhd
@@ -3,6 +3,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity circuito2 is
 	port(a : in bit_vector(0 to 3)
-		   b : in bit_vector(0 to 3)
-		   f : out std_logic);
+	     b : in bit_vector(0 to 3)
+  	     f : out std_logic);
 end circuito2;
