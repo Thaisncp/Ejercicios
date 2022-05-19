27989610a0d92c194d73c3101ef9f2ba55a8da85
Thais Cartuche <93340721+Thaisncp@users.noreply.github.com>
2022-05-18 21:52:00 -0500

    Update Ejercicio11-vector2.vhd

--- Ejercicio11-vector2.vhd
+++ Ejercicio11-vector2.vhd
@@ -3,8 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity vectores2 is
 	port (A : in std_logic_vector(0 to 3);
-			  B : in std_logic_vector(0 to 3);
-			  C : out std_logic_vector(0 to 3));
+	 	B : in std_logic_vector(0 to 3);
+		C : out std_logic_vector(0 to 3));
 end vectores2;
 
 
